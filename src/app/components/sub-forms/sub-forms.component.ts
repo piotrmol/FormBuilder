de98faefb8283e426dc1ec94a059d01b7b50d840
Piotr Mol <piotradammol@gmail.com>
2018-07-18 20:50:28 +0200

    Deleted unused inport

--- src/app/components/sub-forms/sub-forms.component.ts
+++ src/app/components/sub-forms/sub-forms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsService } from '../../services/forms.service';
 import { FormModel } from '../../models/form.model';
 
